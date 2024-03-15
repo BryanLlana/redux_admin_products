e73044fa199f8fed8dfb995eba9dff3b215956c5
Bryan Joseph <bryanllana14@gmail.com>
2024-03-15 12:27:54 -0500

    Header ready

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 p-4">
+      <div className="flex justify-between items-center container mx-auto">
+        <h1 className="text-white text-3xl font-bold">AppProductos</h1>
+        <Link
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          to='/nuevo-producto'
+        >Agregar Producto
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
